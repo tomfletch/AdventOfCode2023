4f1f69a92ef361cb9e8d985bad2326f5baa22671
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2023-12-02 08:01:34 +0000

    Improve day 1 algorithm

--- src/01/day01.ts
+++ src/01/day01.ts
@@ -10,96 +10,61 @@ const NUMBER_TEXT_LOOKUP = {
   seven: 7,
   eight: 8,
   nine: 9,
-  "0": 0,
-  "1": 1,
-  "2": 2,
-  "3": 3,
-  "4": 4,
-  "5": 5,
-  "6": 6,
-  "7": 7,
-  "8": 8,
-  "9": 9,
 }
 
 const part1 = () => {
   const lines = readInputLines(1)
-  const lineNumbers = lines.map(getLineNumber)
+  const lineNumbers = lines.map((line) => getLineNumber(line, false))
   const total = lineNumbers.reduce((a, c) => a + c, 0)
   console.log(total)
 }
 
 const part2 = () => {
   const lines = readInputLines(1)
-  const lineNumbers = lines.map(getLineNumberWithWords)
+  const lineNumbers = lines.map((line) => getLineNumber(line, true))
   const total = lineNumbers.reduce((a, c) => a + c, 0)
   console.log(total)
 }
 
-const getLineNumber = (line: string): number => {
-  const digits = line.replaceAll(/\D+/g, "")
-  const firstDigit = digits[0]
-  const lastDigit = digits[digits.length - 1]
+const getLineNumber = (line: string, withWords: boolean): number => {
+  let numberLookup = Array(10)
+    .fill(0)
+    .map((v, i) => [i.toString(), i] as [string, number])
 
-  if (!firstDigit || !lastDigit) {
-    throw new Error(`No digits in line: ${line}`)
+  if (withWords) {
+    numberLookup = numberLookup.concat(Object.entries(NUMBER_TEXT_LOOKUP))
   }
 
-  const numberStr = `${firstDigit}${lastDigit}`
+  const firstDigit = getFirstDigit(line, numberLookup)
 
-  const number = parseInt(numberStr)
+  const reversedLine = reverseString(line)
+  const reversedNumberLookup = numberLookup.map(
+    ([digitText, digit]) =>
+      [reverseString(digitText), digit] as [string, number],
+  )
 
-  if (!number) {
-    throw new Error(`Failed to parse number: ${numberStr}`)
-  }
-
-  return number
-}
-
-const getLineNumberWithWords = (line: string): number => {
-  const firstDigit = getFirstDigit(line)
-  const lastDigit = getLastDigit(line)
+  const lastDigit = getFirstDigit(reversedLine, reversedNumberLookup)
   return firstDigit * 10 + lastDigit
 }
 
-const getFirstDigit = (line: string): number => {
-  let firstDigit: number | null = null
-  let firstDigitIndex: number = line.length
-
-  for (const [digitText, digit] of Object.entries(NUMBER_TEXT_LOOKUP)) {
-    const digitIndex = line.indexOf(digitText)
-
-    if (digitIndex !== -1 && digitIndex < firstDigitIndex) {
-      firstDigit = digit
-      firstDigitIndex = digitIndex
+const getFirstDigit = (
+  line: string,
+  numberLookup: [string, number][],
+): number => {
+  for (let i = 0; i < line.length; i++) {
+    const subline = line.substring(i)
+    for (const [digitText, digit] of numberLookup) {
+      if (subline.startsWith(digitText)) {
+        return digit
+      }
     }
   }
 
-  if (!firstDigit) {
-    throw new Error(`No first digit on line: ${line}`)
-  }
-
-  return firstDigit
+  throw new Error(`No digit on line: ${line}`)
 }
 
-const getLastDigit = (line: string): number => {
-  let lastDigit: number | null = null
-  let lastDigitIndex: number = -1
-
-  for (const [digitText, digit] of Object.entries(NUMBER_TEXT_LOOKUP)) {
-    const digitIndex = line.lastIndexOf(digitText)
-
-    if (digitIndex !== -1 && digitIndex > lastDigitIndex) {
-      lastDigit = digit
-      lastDigitIndex = digitIndex
-    }
-  }
-
-  if (!lastDigit) {
-    throw new Error(`No last digit on line: ${line}`)
-  }
-
-  return lastDigit
+const reverseString = (str: string): string => {
+  return Array.from(str).reverse().join("")
 }
 
 export default [part1, part2]
