87eb1ff208b038cad112bf3c6e560d4115178c63
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2023-12-03 02:03:58 +0000

    Complete day 2 part 2

--- src/02/day02.ts
+++ src/02/day02.ts
@@ -26,7 +26,15 @@ const part1 = () => {
   console.log(idSum)
 }
 
-const part2 = () => {}
+const part2 = () => {
+  const lines = readInputLines(2)
+  const games = lines.map(parseGame)
+
+  const minSubsets = games.map(getGameMinSubset)
+  const powers = minSubsets.map(getSubsetPower)
+  const powerSum = powers.reduce((acc, p) => acc + p, 0)
+  console.log(powerSum)
+}
 
 const parseGame = (line: string): Game => {
   const [gamePart, subsetsPart] = line.split(":")
@@ -89,6 +97,32 @@ const isValidSubset = (subset: Subset): boolean => {
   }
 
   return true
+}
+
+const getGameMinSubset = (game: Game): Subset => {
+  const minSubset: Subset = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  }
+
+  for (const subset of game.subsets) {
+    if (subset.red > minSubset.red) {
+      minSubset.red = subset.red
+    }
+    if (subset.green > minSubset.green) {
+      minSubset.green = subset.green
+    }
+    if (subset.blue > minSubset.blue) {
+      minSubset.blue = subset.blue
+    }
+  }
+
+  return minSubset
+}
+
+const getSubsetPower = (subset: Subset): number => {
+  return subset.red * subset.green * subset.blue
 }
 
 export default [part1, part2]
